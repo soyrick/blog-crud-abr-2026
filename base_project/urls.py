ef6e5069a30fcfa440793e233e6169687d01620a
soyrick <ricardoenriquegr27@gmail.com>
2026-05-02 11:33:47 -0400

    signup done

--- base_project/urls.py
+++ base_project/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('newspaper.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('signup.urls')),
 ]
