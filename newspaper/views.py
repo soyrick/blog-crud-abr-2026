c0f4c88d69455923f4b4cf0c916de2fd220cd497
soyrick <ricardoenriquegr27@gmail.com>
2026-05-02 11:50:21 -0400

    permissions done

--- newspaper/views.py
+++ newspaper/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (ListView, 
                                 DetailView, 
                                 CreateView,
@@ -14,17 +15,17 @@ class ArticleDetailView(DetailView):
     model = Article
     template_name = 'article-detail.html'
     
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     template_name = 'article-create.html'
     fields = ['title', 'content', 'author']
 
-class ArticleUpdateView(UpdateView):  
+class ArticleUpdateView(LoginRequiredMixin, UpdateView):  
     model = Article
     template_name = 'article-update.html'
     fields = ['title', 'content',]
     
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin, DeleteView):
     model = Article
     template_name = 'article-delete.html'
     success_url = reverse_lazy('article-list')
