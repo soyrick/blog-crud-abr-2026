13602b61bc904c0bae391f3f810eef3850c46eab
soyrick <ricardoenriquegr27@gmail.com>
2026-05-02 09:52:07 -0400

    Edit done

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ArticleListView, ArticleDetailView, ArticleCreateView
+from .views import ArticleListView, ArticleDetailView, ArticleCreateView, ArticleUpdateView
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='article-list'),
     path('articulos/<int:pk>/', ArticleDetailView.as_view(), name='article-detail'),
     path('articulos/crear/', ArticleCreateView.as_view(), name='article-create'),
+    path('articulos/<int:pk>/editar/', ArticleUpdateView.as_view(), name='article-update'),
 ]
--- newspaper/views.py
+++ newspaper/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import (ListView, 
                                 DetailView, 
-                                CreateView)
+                                CreateView,
+                                UpdateView,)
 from .models import Article
 # Create your views here.
 class ArticleListView(ListView):
@@ -15,3 +16,8 @@ class ArticleCreateView(CreateView):
     model = Article
     template_name = 'article-create.html'
     fields = ['title', 'content', 'author']
+
+class ArticleUpdateView(UpdateView):  
+    model = Article
+    template_name = 'article-update.html'
+    fields = ['title', 'content',]
